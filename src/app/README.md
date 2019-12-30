ca05ada40a3ef3b20303691efd6e93f0afc1bd52
DanielGabay <danielgabay5@gmail.com>
2019-12-30 12:17:21 +0200

    continue working on main.py, adding readme

---
+++ src/app/README.md
@@ -0,0 +1,5 @@
+Create 'data' folder and put .tiff documents inside.
+Create models folder and put the .json .h5 files of the model
+
+Run:
+python main.py <file_name in data folder>
--- src/app/main.py
+++ src/app/main.py
@@ -9,49 +9,56 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+DATA_PATH = "data/"
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 hebrew_letters = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י'\
             ,'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',\
            'ך', 'ם', 'ן', 'ף', 'ץ']
 
-def main():
-    if(len(sys.argv) < 2):
-        print("Usage: python detectLines.py <file_name>")
-        sys.exit(1)
-
-
-
-    img_name = "data/" + str(sys.argv[1])
-    img = get_prepared_doc(img_name)
-    lines = get_lines(img, img_name)
-    letters = get_letters(lines)
-
-
-    num_of_classes = 27
-    image_size = 28  # 28X28 pixels
-    batch_Size = 128
-    # load json and create model
-    json_file = open('model99.json', 'r')
+def load_and_compile_model():
+    json_file = open('models/model99.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     classifier = model_from_json(loaded_model_json)
-    classifier.load_weights("model99.h5")
-    print("Loaded model from disk")
-    classifier.compile(optimizer='adam',  loss='categorical_crossentropy', metrics=['accuracy'])
-    text = ""
-    count_good = 0
-    count_all = 0
+    classifier.load_weights("models/model99.h5")
+    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    print("Loaded & compiled model from disk")
+    return classifier
+
+def save_letters(letters, classifier):
     for letter in letters:
-        letter = cv2.resize(letter.image_letter, (28, 28)) ######## this we addedddddddddddddddddd
-        letter = letter.reshape((28, 28,1))######## this we addedddddddddddddddddd
+        letter = cv2.resize(letter.image_letter, (28, 28))
+        letter = letter.reshape((28, 28, 1))
 
         test_letter = image.img_to_array(letter)
         test_image = np.expand_dims(test_letter, axis=0)
         result = classifier.predict((test_image/255))
-        # for i, v in enumerate(result[0]):
-        #     print(str(i)+" " + hebrew_letters[i]+": "+str(float("{0:.2f}".format(v))))
-        # print("______")
+        if max(result[0]) > 0.995:
+            selected_letter = hebrew_letters[result[0].tolist().index(max(result[0]))]
+            if selected_letter == "ץ": 
+                continue
+            resized = cv2.resize(letter, (50,50), interpolation = cv2.INTER_AREA)
+            cv2.imshow('',resized)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+def print_predictions(preidction):
+        for i, v in enumerate(preidction):
+            print(str(i)+" " + hebrew_letters[i]+": "+str(float("{0:.2f}".format(v))))
+        print("______")
+
+def show_letters(letters, classifier):
+    count_good = 0
+    count_all = 0
+    for letter in letters:
+        letter = cv2.resize(letter.image_letter, (28, 28))
+        letter = letter.reshape((28, 28, 1))
+
+        test_letter = image.img_to_array(letter)
+        test_image = np.expand_dims(test_letter, axis=0)
+        result = classifier.predict((test_image/255))
+        # print_predictions(result[0])
         if max(result[0]) > 0.995:
             selected_letter = hebrew_letters[result[0].tolist().index(max(result[0]))]
             if selected_letter == "ץ": 
@@ -70,10 +77,18 @@ def main():
             plt.close('all')
     print("{} {} {}".format(count_good, count_all, count_good/count_all))
 
-    # print(lines)
-    # for line in lines:
-    #     cv2.imshow('line', line)
-    #     cv2.waitKey(0)
+def main():
+    if(len(sys.argv) < 2):
+        print("Usage: python main.py <file_name>")
+        sys.exit(1)
+
+    img_name = DATA_PATH + str(sys.argv[1])
+    img = get_prepared_doc(img_name)
+    lines = get_lines(img, img_name)
+    letters = get_letters(lines)
+    classifier = load_and_compile_model()
+    show_letters(letters, classifier)
+    # save_letters(letters, classifier)    
 
 if __name__ == "__main__":
     main()
