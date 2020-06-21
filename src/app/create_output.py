f10e40303507685bdb1789e38867b50569460076
Shachar Israeli <israelish@post.jce.ac.il>
2020-06-21 19:39:08 +0300

    refactoring

--- src/app/create_output.py
+++ src/app/create_output.py
@@ -34,3 +34,25 @@ def generate_conclusion(compare_docs):
 	# conclusion2 += compare_docs.monkey_results['result'] + ">" if\
 	# 			  compare_docs.monkey_results['result'] == compare_docs.letters_ae_results['result_by_predictions']\
 	# 			  else "Conflict>" 
+
+
+def print_ae_monkey_results(s, len_b):
+	print("\n------------------")
+	print("Number of same pairs checked:{}".format(len_b))
+	print("Sum of al pairs checked: {}".format(s.count_num_of_tests))
+	print("\n------------------")
+	print_conf_matrix("Monkey & letter AE Conf & ssim Matrix:", s.tn, s.tp, s.fn, s.fp)
+	print("Model accuracy: {0:.2f}%".format(model_acc(s.tn, s.tp, s.fn, s.fp)))
+	# print_conf_matrix("Monkey & letter AE Conf Matrix:", s.tn, s.tp, s.fn, s.fp)
+	# print("Model accuracy: {0:.2f}% (*NOTE: not includes Undecided results!)".format(model_acc(s.tn, s.tp, s.fn, s.fp)))
+	# print("Undecided Results:\n->conflict:{}\n-->conflict_while_same:{}\n-->conflict_while_diff:{}"\
+		# .format(s.conflict, s.conflict_while_same,s.conflict_while_diff))
+	print("\n------------------")
+	print_conf_matrix("Only letter AE Conf Matrix:", s.ae_tn, s.ae_tp, s.ae_fn, s.ae_fp)
+	print("Model accuracy: {0:.2f}%".format(model_acc(s.ae_tn, s.ae_tp, s.ae_fn, s.ae_fp)))
+	print("\n------------------")
+	print_conf_matrix("Only Monkey Conf Matrix:", s.monkey_tn, s.monkey_tp, s.monkey_fn, s.monkey_fp)
+	print("Model accuracy: {0:.2f}%".format(model_acc(s.monkey_tn, s.monkey_tp, s.monkey_fn, s.monkey_fp)))
+	print("\n------------------")
+	print_conf_matrix("Only ssim Conf Matrix:", s.ssim_tn, s.ssim_tp, s.ssim_fn, s.ssim_fp)
+	print("Model accuracy: {0:.2f}%".format(model_acc(s.ssim_tn, s.ssim_tp, s.ssim_fn, s.ssim_fp)))
--- src/app/main.py
+++ src/app/main.py
@@ -7,7 +7,7 @@ import eel
 import _global
 from classes import CompareDocuments, Document, Stats
 from prepare_document import get_prepared_doc
-from create_output import generate_conclusion,generate_gui_output,generate_output
+from create_output import generate_conclusion,generate_gui_output,generate_output,print_ae_monkey_results
 
 #Recognition Phase
 from recognition_functions import (get_identified_letters,
@@ -177,26 +177,6 @@ def model_acc(tn, tp, fn, fp):
 		return 0
 	return (tn+tp)/(total) * 100
 
-def print_ae_monkey_results(s, len_b):
-	print("\n------------------")
-	print("Number of same pairs checked:{}".format(len_b))
-	print("Sum of al pairs checked: {}".format(s.count_num_of_tests))
-	print("\n------------------")
-	print_conf_matrix("Monkey & letter AE Conf & ssim Matrix:", s.tn, s.tp, s.fn, s.fp)
-	print("Model accuracy: {0:.2f}%".format(model_acc(s.tn, s.tp, s.fn, s.fp)))
-	# print_conf_matrix("Monkey & letter AE Conf Matrix:", s.tn, s.tp, s.fn, s.fp)
-	# print("Model accuracy: {0:.2f}% (*NOTE: not includes Undecided results!)".format(model_acc(s.tn, s.tp, s.fn, s.fp)))
-	# print("Undecided Results:\n->conflict:{}\n-->conflict_while_same:{}\n-->conflict_while_diff:{}"\
-		# .format(s.conflict, s.conflict_while_same,s.conflict_while_diff))
-	print("\n------------------")
-	print_conf_matrix("Only letter AE Conf Matrix:", s.ae_tn, s.ae_tp, s.ae_fn, s.ae_fp)
-	print("Model accuracy: {0:.2f}%".format(model_acc(s.ae_tn, s.ae_tp, s.ae_fn, s.ae_fp)))
-	print("\n------------------")
-	print_conf_matrix("Only Monkey Conf Matrix:", s.monkey_tn, s.monkey_tp, s.monkey_fn, s.monkey_fp)
-	print("Model accuracy: {0:.2f}%".format(model_acc(s.monkey_tn, s.monkey_tp, s.monkey_fn, s.monkey_fp)))
-	print("\n------------------")
-	print_conf_matrix("Only ssim Conf Matrix:", s.ssim_tn, s.ssim_tp, s.ssim_fn, s.ssim_fp)
-	print("Model accuracy: {0:.2f}%".format(model_acc(s.ssim_tn, s.ssim_tp, s.ssim_fn, s.ssim_fp)))
 
 def get_doc_by_name(all_docs, file_name):
 	for doc in all_docs:
